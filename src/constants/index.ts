cbfdf8ac7e1ec47bb7949ebced621e0575abd5ac
Asaiy420 <kcaryan05@gmail.com>
2026-03-06 00:45:03 +0545

    feat: add departments and department options constants

---
+++ src/constants/index.ts
@@ -0,0 +1,12 @@
+export const DEPARTMENTS = [
+  'Computer Science',
+  'Mathematics',
+  'Physics',
+  'Chemistry',
+  'Humanities',
+];
+
+export const DEPARTMENT_OPTIONS = DEPARTMENTS.map(dept => ({
+  value: dept,
+  label: dept,
+}));
